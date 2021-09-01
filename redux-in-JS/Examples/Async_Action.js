f870e6f75a978bde5a5fb6cac4c87bed80ec28bb
Mdakhib <mohamedakhib@gmail.com>
2021-09-01 15:34:25 +0530

    reducer added with switch case which will actions perform by TYPE by fetching in actions

--- redux-in-JS/Examples/Async_Action.js
+++ redux-in-JS/Examples/Async_Action.js
@@ -21,9 +21,33 @@ const fetchUsersSuccess = (users) => {
   };
 };
 
-const fetchUsersFailure = (error) => {
+const fetchUsersFailure = error => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error,
   };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
 };
