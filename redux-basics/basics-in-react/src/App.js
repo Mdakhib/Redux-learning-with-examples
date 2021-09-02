a511be7699a07982cd730c2dea75f62acd98c7f7
Mdakhib <mohamedakhib@gmail.com>
2021-09-02 15:31:09 +0530

    instead of using connect HOF's we can do that by using hooks called useSelector which perform same operation as the mapStateToProps function

--- redux-basics/basics-in-react/src/App.js
+++ redux-basics/basics-in-react/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Provider } from "react-redux";
-import CakeContainer from "./components/CakeContainer";
+// import CakeContainer from "./components/CakeContainer";
 import store from "./redux/store";
+import HookCakeContainer from './components/HookCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        {/* by using connect HOF's */}
+        {/* <CakeContainer /> */}
+        {/* ------------------------------------------------------------------------- */}
+        <HookCakeContainer />
       </div>
     </Provider>
   );
