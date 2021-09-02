dc368d61fb99b681a53564aa537c765f52e11e01
Mdakhib <mohamedakhib@gmail.com>
2021-09-02 15:03:13 +0530

    we have connected by using connect func from redux in which thr will be state nd dispatch function at last it will connected with component

--- redux-basics/basics-in-react/src/components/CakeContainer.js
+++ redux-basics/basics-in-react/src/components/CakeContainer.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
 
-export default function CakeContainer() {
-    return (
-      <div>
-        <h2>Number of Cakes</h2>
-        <button>Buy Cake</button>
-      </div>
-    );
+function CakeContainer({ buyCake, numOfCakes }) {
+  return (
+    <div>
+      <h2>Number of Cakes - {numOfCakes} </h2>
+      <button onClick={buyCake}>Buy Cake</button>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchProps)(CakeContainer);
