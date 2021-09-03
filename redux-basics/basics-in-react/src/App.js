920062a17096248d1f88026a3ca9851f5e2ed4d8
Mdakhib <mohamedakhib@gmail.com>
2021-09-03 15:55:47 +0530

    actions and reducer added to fetch an API

--- redux-basics/basics-in-react/src/App.js
+++ redux-basics/basics-in-react/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { Provider } from "react-redux";
 // import CakeContainer from "./components/CakeContainer";
 import store from "./redux/store";
+import UserContainer from "./components/UserContainer";
 // import HookCakeContainer from './components/HookCakeContainer';
 // import IceCreamContainer from './components/IceCreamContainer';
 // import CounterContainer from './components/CounterContainer';
 // import CakeContainerwithPayload from './components/CakeContainerwithPayload';
-import ItemContainer from './components/ItemContainer';
+// import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
@@ -24,8 +25,11 @@ function App() {
         {/* <HookCakeContainer /> */}
         {/* ------------------------------------------------------------------------- */}
         {/* using ownProps in mapStateToProps */}
-        <ItemContainer propsPassingByChildComponentToParentAsOwnProps />
-        <ItemContainer  />
+        {/* <ItemContainer propsPassingByChildComponentToParentAsOwnProps /> */}
+        {/* <ItemContainer  /> */}
+        {/* ------------------------------------------------------------------------- */}
+        {/* Fetching an API  */}
+        <UserContainer />
       </div>
     </Provider>
   );
--- redux-basics/basics-in-react/src/redux/index.js
+++ redux-basics/basics-in-react/src/redux/index.js
@@ -1,3 +1,5 @@
 export { buyCake } from "./cakes/cakeActions";
 export { buyIceCream } from "./iceCream/iceCreamActions";
 export { incrementCounter, decrementCounter } from "./counter/couterActions";
+
+export * from "./user/UserActions";
