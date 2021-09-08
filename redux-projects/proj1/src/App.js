285908dec9a4a2ac54feb6857b6dd2f5ae96fd21
Mdakhib <mohamedakhib@gmail.com>
2021-09-08 11:21:49 +0530

    components added nd we r using hook of redux

--- redux-projects/proj1/src/App.js
+++ redux-projects/proj1/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ProductListing} />
+          <Route exact path="/product/:productId" component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
