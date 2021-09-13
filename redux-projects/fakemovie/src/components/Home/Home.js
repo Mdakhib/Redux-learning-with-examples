9db7f57b529d1fee10158e7f4b11b8f6a1e3f78b
Mdakhib <mohamedakhib@gmail.com>
2021-09-13 14:21:59 +0530

    data fetched through api

--- redux-projects/fakemovie/src/components/Home/Home.js
+++ redux-projects/fakemovie/src/components/Home/Home.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-
+import movieApi from "../../common/apis/movieApi";
+import { APIKEY } from "../../common/apis/movieApiKey";
 const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKEY}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Err", err);
+        });
+      console.log("the response from API: ", response);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <div>
       <div className="banner-img"></div>
