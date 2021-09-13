271f70eff2258f4b8740b9d630740859d59952b3
Mdakhib <mohamedakhib@gmail.com>
2021-09-13 15:51:35 +0530

    store created by using redux-toolkit in which we created by using configureStore and created createSlice to store actions and reducer at one file

---
+++ redux-projects/fakemovie/src/features/movies/movieSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  movies: [],
+};
+
+const movieSlice = createSlice({
+  name: "movie", //slice name
+  initialState,
+  reducers: {
+    addMovies: (state, { payload }) => {
+      state.movies = payload;
+    },
+  },
+});
+
+export const { addMovies } = movieSlice.actions;
+export const getAllMovies = (state) => state.movie.movies;
+export default movieSlice.reducer;
